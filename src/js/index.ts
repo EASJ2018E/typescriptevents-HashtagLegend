ed9248043c7a2bb56978e0343bbff89a0fdc03a8
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-10-01 20:53:35 +0200

    Added eventlistener add li object to list

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,12 @@
-//Her skal du skrive din typescriptkode
+var button = document.getElementById('knap')
+button.addEventListener('click', addFunction)
+
+function addFunction(){
+  let i = document.createElement('li')
+  let inputText = (<HTMLInputElement>document.getElementById('input')).value
+  let it = document.createTextNode(inputText)
+  i.appendChild(it)
+  i.setAttribute("id", "5")
+  let l = document.getElementById('listeUsund')
+  l.appendChild(i)
+}
