64022e15138992dd015af72a671f721ac17583f7
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-10-02 16:07:09 +0200

    added new branch

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,12 @@
 var button = document.getElementById('knap')
 button.addEventListener('click', addFunction)
 
-function addFunction(){
+function addFunction() : void{
   let i = document.createElement('li')
   let inputText = (<HTMLInputElement>document.getElementById('input')).value
   let it = document.createTextNode(inputText)
   i.appendChild(it)
-  i.setAttribute("id", "5")
+
   let l = document.getElementById('listeUsund')
   l.appendChild(i)
 }
