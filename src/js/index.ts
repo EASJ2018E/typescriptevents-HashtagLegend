619ea95ad930a2c5cfe2c51a59171681ad69df94
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-10-09 19:45:10 +0200

    testet classes

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,23 @@
 var button = document.getElementById('knap')
 button.addEventListener('click', addFunction)
 
+class Greeter {
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet(){
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("World")
+
+
 function addFunction(){
+  console.log(greeter)
   let i = document.createElement('li')
   let inputText = (<HTMLInputElement>document.getElementById('input')).value
   let it = document.createTextNode(inputText)
@@ -10,3 +26,5 @@ function addFunction(){
   let l = document.getElementById('listeUsund')
   l.appendChild(i)
 }
+
+console.log(greeter)
